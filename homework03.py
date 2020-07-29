08711a3d4627448adc531f877ccc338f0ed9b98a
jiangzhixiu <49977672+jiangzhixiu@users.noreply.github.com>
2020-07-29 11:13:56 +0900

    insert mogodb

--- homework03.py
+++ homework03.py
@@ -1,7 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+client = MongoClient('localhost', 27017)
+db = client.jisuhomework
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713',headers=headers)
 
 soup = BeautifulSoup(data.text, 'html.parser')
@@ -11,3 +15,6 @@ for tr in trs:
     title = tr.select_one('td.info > a.title.ellipsis').text.strip()
     name = tr.select_one('td.info > a.artist.ellipsis').text
     print(rank,title,name)
+    
+    music = {'rank': rank, 'title': title, 'name': name}
+    db.users.insert_one(music)
